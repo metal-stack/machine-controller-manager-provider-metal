056c49a289921d092f3a43050a2971b502f24f3f
Stefan Majer <stefan.majer@f-i-ts.de>
2023-04-20 07:49:51 +0200

    Fix json tags

--- pkg/provider/apis/provider_spec.go
+++ pkg/provider/apis/provider_spec.go
@@ -1,13 +1,12 @@
 package api
 
 // MetalProviderSpec is the spec to be used while parsing the calls.
-// nolint:musttag
 type MetalProviderSpec struct {
-	Partition string // required
-	Size      string // required
-	Image     string // required
-	Project   string // required
-	Network   string // required
-	Tags      []string
-	SSHKeys   []string
+	Partition string   `json:"partition,omitempty"` // required
+	Size      string   `json:"size,omitempty"`      // required
+	Image     string   `json:"image,omitempty"`     // required
+	Project   string   `json:"project,omitempty"`   // required
+	Network   string   `json:"network,omitempty"`   // required
+	Tags      []string `json:"tags,omitempty"`
+	SSHKeys   []string `json:"sshKeys,omitempty"`
 }
